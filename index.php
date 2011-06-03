e45c73ab66f27f8853208caceaac9aa1727f83f8
Adam J. McIntyre <adamjmcintyre@gmail.com>
2011-06-03 14:50:41 -0400

    Added a \"fork me\" banner

--- index.php
+++ index.php
@@ -75,5 +75,7 @@ include_once('layout/header.php');
 		<script src="js/libs/dd_belatedpng.js"></script>
 		<script>DD_belatedPNG.fix("img, .png_bg"); // Fix any <img> or .png_bg bg-images. Also, please read goo.gl/mZiyb </script>
 		<![endif]-->
+		
+		<a href="https://github.com/isobar-idev/code-standards/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://d3nwyuy0nl342s.cloudfront.net/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
 	</body>
 </html>
