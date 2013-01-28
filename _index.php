d843e7c00673647f1677cd367a66220370352c8c
Rob Cherny <rcherny@gmail.com>
2013-01-27 20:46:53 -0500

    Merged files from the public live master branch. i don't think this breaks anything. still have to review. The JS file needs some love, but I think that is the last bit that needs to be handled to cover all the merge disasters from the last 9 months. Long road to "recovery" may at last be within reach. Sigh … Ok, no, we still have tons of inconsistencies, old and new files, things that need to be removed, but at least the content and CSS is handled now.

---
+++ _index.php
@@ -0,0 +1,7 @@
+<?php
+// 301 Moved Permanently
+// header("Location: http://isobar-idev.github.com/code-standards/",TRUE,301);
+
+// 302 Found
+header("Location: http://isobar-idev.github.com/code-standards/",TRUE,302);
+?>
