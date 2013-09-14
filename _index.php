9111007b4e40b963ad5d060c791dfd4aa033fd20
Rob Cherny <rcherny@gmail.com>
2013-09-14 17:40:12 -0400

    Removing includes folder and adding THE DINOSAUR to the main layout file as it is no longer in the includes. Addressing part of #60 and #64

--- _index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// 301 Moved Permanently
-// header("Location: http://isobar-idev.github.com/code-standards/",TRUE,301);
-
-// 302 Found
-header("Location: http://isobar-idev.github.com/code-standards/",TRUE,302);
-?>
